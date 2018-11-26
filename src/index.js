925dda240c3839e4ca2f55c800acfadb6c308356
jenifercoila <jeniferfcr@gmail.com>
2018-11-26 09:55:36 -0500

    Terminando de agregar objetos

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ document.getElementById("start").addEventListener("click", function(){
     document.getElementById("first-page-title").style.display = "none";
     });
 
+
+let ciphertext = document.getElementById("cipherbutton").addEventListener("click", function() {
+
+let offseten = parseInt(document.getElementById("offsetin").value);
+let messageen = document.getElementById("messagetocipher").value;
+let messageentrance= cipher.encode(messageen,offseten);
+console.log(messageentrance);
+
+
+
+document.getElementById("result").innerHTML = 3 ;
+});
+
+
