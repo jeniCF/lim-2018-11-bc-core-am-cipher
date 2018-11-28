3ea77dab08eaf90a015161a6ae6a9b78f4da6b3b
jenifercoila <jeniferfcr@gmail.com>
2018-11-28 17:24:22 -0500

    titulos de css y fondo hecho

--- src/cipher.js
+++ src/cipher.js
@@ -5,10 +5,18 @@ const cipher_ = (message,offset) => {
   let ascii_to_cipher = [];
   
   for(let i =0 ; i< message.length; i++) {
-    to_codeascii.push(message.charCodeAt(i));
-    applying_offset.push((to_codeascii[i] + offset -65)%26 +65);
-    ascii_to_cipher.push(String.fromCharCode(applying_offset[i]));
+
+    let ascii= to_codeascii.push(message.charCodeAt(i));
+    
+    if (to_codeascii[i] === 32) {
+    applying_offset[i] = [32];
     }
+    
+    else {applying_offset.push((to_codeascii[i] + offset -65)%26 +65);
+    }
+    ascii_to_cipher.push(String.fromCharCode(applying_offset[i]));
+
+  }
     let strascii= ascii_to_cipher.join ("");
     return (strascii)
   }
@@ -21,7 +29,13 @@ const cipher_ = (message,offset) => {
   
   for (let i=0; i <messagede.length; i++) {
     
-  tocodeascii_de.push(messagede.charCodeAt(i));
+  let ascii= tocodeascii_de.push(messagede.charCodeAt(i));
+
+if (tocodeascii_de[i] === 32) {
+  applying_offset_de[i] = [32];
+  }
+
+  else {
 
 if (Math.sign((tocodeascii_de[i]-offsetde -65)%26)>=0)  {
   applying_offset_de.push((tocodeascii_de[i]-offsetde -65)%26 +65);
@@ -31,8 +45,8 @@ else {
   }
      /*applying_offset_de.push((Math.abs(tocodeascii_de[i]-offsetde -65))%26 +65);
     26 - math.abs todo ) +65 <= todo para ejemplos negativos */
+  }
   ascii_to_cipher_de.push(String.fromCharCode(applying_offset_de[i]));
-
     }
     let straasciide= ascii_to_cipher_de.join("");
     return (straasciide)
--- src/index.js
+++ src/index.js
@@ -6,22 +6,28 @@ document.getElementById("start").addEventListener("click", function(){
     document.getElementById("main-page").style.display = "block";
     document.getElementById("first-page-title").style.display = "none";
     });
-       
-document.getElementById("cipherbutton").addEventListener("click", function() { 
+  
+ let not_negative = parseInt(document.getElementById("offsetin").value);
+
+ if (not_negative< 0) {
+        alert ("Debe seleccionar un número mayor que 0");
+        }
+    else {
+
+    document.getElementById("cipherbutton").addEventListener("click", function() { 
    let offseten = parseInt(document.getElementById("offsetin").value);
-   if (offseten < 0 ) {
-   alert ("Debe seleccionar un número mayor que 0")
-   }
-   else {
-    let messageen = document.getElementById("messagetocipher").value;
+   
+    let messageen = (document.getElementById("messagetocipher").value).toUpperCase();
     let messageentrance= cipher.encode(messageen,offseten);
-    document.getElementById("result").value += messageentrance;
-   }
+    document.getElementById("result").value = messageentrance;
 });
+    }
+
 
 document.getElementById("decipherbutton").addEventListener("click", function() {
     let offseten = parseInt(document.getElementById("offsetin").value);    
-    let messageen = document.getElementById ("messagetodecipher").value;
+    let messageen = (document.getElementById ("messagetodecipher").value).toUpperCase();
+
     let messageentrance= cipher.decode(messageen,offseten);
     document.getElementById("result").value = messageentrance;
     });
