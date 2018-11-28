382a8e2b891412e95adcd5ea8e81be5ad6cf03c0
jenifercoila <jeniferfcr@gmail.com>
2018-11-27 19:23:57 -0500

    Dia finalizado, agregando css

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 document.getElementById("main-page").style.display = "none";
     
-    
+
 document.getElementById("start").addEventListener("click", function(){
     event.preventDefault();
     document.getElementById("main-page").style.display = "block";
     document.getElementById("first-page-title").style.display = "none";
     });
-
-
-document.getElementById("cipherbutton").addEventListener("click", function() {
-
-let offseten = parseInt(document.getElementById("offsetin").value);
-let messageen = document.getElementById("messagetocipher").value;
-let messageentrance= cipher.encode(messageen,offseten);
-document.getElementById("result").value += messageentrance;
+       
+document.getElementById("cipherbutton").addEventListener("click", function() { 
+   let offseten = parseInt(document.getElementById("offsetin").value);
+   if (offseten < 0 ) {
+   alert ("Debe seleccionar un número mayor que 0")
+   }
+   else {
+    let messageen = document.getElementById("messagetocipher").value;
+    let messageentrance= cipher.encode(messageen,offseten);
+    document.getElementById("result").value += messageentrance;
+   }
 });
 
 document.getElementById("decipherbutton").addEventListener("click", function() {
-
-    let offseten = parseInt(document.getElementById("offsetin").value);
-    let messageen = document.getElementById("messagetodecipher").value;
+    let offseten = parseInt(document.getElementById("offsetin").value);    
+    let messageen = document.getElementById ("messagetodecipher").value;
     let messageentrance= cipher.decode(messageen,offseten);
     document.getElementById("result").value = messageentrance;
     });
@@ -27,8 +29,8 @@ document.getElementById("decipherbutton").addEventListener("click", function() {
 
 document.getElementById("clean").addEventListener("click",function (){
      let clean = "";    
-    document.getElementById("messagetocipher").value = clean;
-        document.getElementById("messagetodecipher").value= clean; 
-        document.getElementById("result").value = clean;
+    document.getElementById("messagetocipher").value= clean; 
+    document.getElementById ("messagetodecipher").value= clean;
+    document.getElementById("result").value = clean;
     });
 
