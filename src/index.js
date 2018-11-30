9e7e930c52a2ebddc8be5f51f1242bd61c91f892
jenifercoila <jeniferfcr@gmail.com>
2018-11-29 20:23:40 -0500

    Terminando css

--- src/index.js
+++ src/index.js
@@ -7,29 +7,32 @@ document.getElementById("start").addEventListener("click", function(){
     document.getElementById("first-page-title").style.display = "none";
     });
   
- let not_negative = parseInt(document.getElementById("offsetin").value);
-
- if (not_negative< 0) {
-        alert ("Debe seleccionar un número mayor que 0");
-        }
-    else {
 
     document.getElementById("cipherbutton").addEventListener("click", function() { 
-   let offseten = parseInt(document.getElementById("offsetin").value);
-   
+
+   if (document.getElementById("offsetin").value === "") {
+    alert ("Debe seleccionar un número");
+    }
+   else {
+    let offseten= parseInt(document.getElementById("offsetin").value);
     let messageen = (document.getElementById("messagetocipher").value).toUpperCase();
     let messageentrance= cipher.encode(messageen,offseten);
     document.getElementById("result").value = messageentrance;
+   }
 });
-    }
 
 
 document.getElementById("decipherbutton").addEventListener("click", function() {
+    if(document.getElementById("offsetin").value === ""){
+    alert ("Debe seleccionar un número");
+    }
+   else {
     let offseten = parseInt(document.getElementById("offsetin").value);    
-    let messageen = (document.getElementById ("messagetodecipher").value).toUpperCase();
+    let messageen = (document.getElementById ("result").value).toUpperCase();
 
     let messageentrance= cipher.decode(messageen,offseten);
-    document.getElementById("result").value = messageentrance;
+    document.getElementById("messagetodecipher").value = messageentrance;
+   }
     });
 
 
@@ -38,5 +41,6 @@ document.getElementById("clean").addEventListener("click",function (){
     document.getElementById("messagetocipher").value= clean; 
     document.getElementById ("messagetodecipher").value= clean;
     document.getElementById("result").value = clean;
+    document.getElementById("offsetin").value=clean;
     });
 
